397db3e6f0908327eacc797d8d0044074f4ad75e
udbbbn <udbbbn@163.com>
2019-03-05 18:34:39 +0800

    动态渲染城市 跟 部分天气(未完成)

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -3,18 +3,16 @@ import 'antd/lib/breadcrumb/style';
 import {Breadcrumb} from 'antd';
 import BreadcrumbItem from 'antd/lib/breadcrumb/BreadcrumbItem';
 import jsonp from "jsonp";
-import axios from 'axios';
 import CryptoJS from 'crypto-js';
 
 import './index.less';
 
-interface location {
-  address?: string,
-  address_detail?: any,
-  point?: object
+interface Istate {
+  location: any, // 接口直接any
+  weather: any
 }
 
-export default class content extends Component<{}, {}> {
+export default class content extends Component<{}, Istate> {
   // 知心天气API
   private API:string = "https://api.seniverse.com/v3/weather/now.json";
   // private userId = "UDBA707165";
@@ -26,7 +24,7 @@ export default class content extends Component<{}, {}> {
   // 并将加密结果用 base64 编码，并做一个 urlencode，得到签名 sig
   private sig:string  = CryptoJS.HmacSHA1(this.str, this.KEY).toString(CryptoJS.enc.Base64);
   // private sig = encodeURIComponent(this.sig);
-  public location:location = {};
+  public location:object = {};
   public weather:object = {};
   // 百度地图API
   private AK: string = "aUoynAD2ywl2KpslltOQ0etEFEsqzj9i";
@@ -36,10 +34,14 @@ export default class content extends Component<{}, {}> {
     super(props);
     this.sig = encodeURIComponent(this.sig);
     this.str = this.str + "&sig=" + this.sig;
+    this.state = {
+      location: {},
+      weather: {}
+    }
   }
 
   /**
-   * 调用百度地图接口获取位置
+   * 获取当前ip的地理位置
    */
   getGeoLocation() {
     let vm:this = this;
@@ -47,7 +49,10 @@ export default class content extends Component<{}, {}> {
       if (err) {
         alert('位置获取失败');
       } else {
-        vm.location = res.content;
+        vm.setState({
+          location: res.content
+        })
+        console.log(vm.state.location);
         vm.getWeather();
       }
     })
@@ -58,14 +63,16 @@ export default class content extends Component<{}, {}> {
    */
   getWeather() {
     let vm:this = this;
-    let location = this.location.address_detail.city;
+    let location = vm.state.location.address_detail.city;
     let api:string = `${this.API}?location=${location}&${this.str}`;
     jsonp(`${api}`,(err, res) => {
       if (err) {
         alert('天气获取失败');
       } else {
-        vm.weather = res.results[0];
-        console.log(vm.weather);
+        vm.setState({
+          weather: res.results[0]
+        })
+        console.log(vm.state.weather);
       }
     });
   }
@@ -74,15 +81,46 @@ export default class content extends Component<{}, {}> {
     this.getGeoLocation();
   }
   
+
+  renderCity() {
+    if (this.state.location['address_detail']) {
+      return (
+        <>
+          <Breadcrumb separator=">" className="fs20">
+            <BreadcrumbItem>{this.state.location.address_detail.province}</BreadcrumbItem>
+            <BreadcrumbItem>{this.state.location.address_detail.city}</BreadcrumbItem>
+          </Breadcrumb>
+        </>
+      );
+    }
+  }
+
+  renderWeather() {
+    if (this.state.weather['location']) {
+      let code = `/src/assets/img/${this.state.weather.now.code}.png`;
+      return (
+        <>
+          <div className="c-weather">
+            <div className="c-weather-top">
+              <div className="c-weather-top-number fs150">
+                {this.state.weather.now.temperature} 
+                <span>°</span>
+              </div>
+              <img src={code} alt="" className="c-weather-top-img" />
+            </div>
+          </div>
+        </>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="c-page">
         <div className="c-nav">
-          <Breadcrumb separator=">" className="fs20">
-            <BreadcrumbItem>广东</BreadcrumbItem>
-            <BreadcrumbItem>深圳</BreadcrumbItem>
-          </Breadcrumb>
+            {this.renderCity()}
         </div>
+        {this.renderWeather()}
       </div>
     )
   }
