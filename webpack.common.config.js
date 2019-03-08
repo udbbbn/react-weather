4b513768393c451eebfa0346b78f9e150a02215d
udbbbn <udbbbn@163.com>
2019-03-08 14:09:20 +0800

    完成配置

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -20,46 +20,6 @@ module.exports = {
                 test: /\.tsx?$/,
                 loader: "ts-loader" 
             },
-            // {
-            //     test: /(\.less)$/,
-            //     exclude: /node_modules|antd\.less/,
-            //     include:path.resolve('src/'),
-            //     use: [
-            //         {
-            //             loader: "style-loader"
-            //         },
-            //         {
-            //             loader: "css-loader"
-            //         },
-            //         {
-            //             loader: "less-loader",
-            //             options: {
-            //                 javascriptEnabled: true
-            //             }
-            //         }
-            //     ]
-            // },
-            {
-                test: /(\.less)$/,
-                exclude: /node_modules|antd\.less/,
-                include:path.resolve('src/'),
-                use: ExtractTextPlugin.extract({
-                    fallback: {
-                        loader: "style-loader"
-                    },
-                    use: [
-                        {
-                            loader: "css-loader"
-                        },
-                        {
-                            loader: "less-loader",
-                            options: {
-                                javascriptEnabled: true
-                            }
-                        }
-                    ]
-                })
-            },
             {
                 test: /(\.less|\.css)$/,
                 include: /node_modules|antd\.less/,
@@ -78,6 +38,17 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit:8192,//限制打包图片的大小：
+                    //如果大于或等于8192Byte，则按照相应的文件名和路径打包图片；如果小于8192Byte，则将图片转成base64格式的字符串。
+                    name:'/assets/img/[name]-[hash:8].[ext]',//img:图片打包的文件夹；
+                    //[name].[ext]：设定图片按照本来的文件名和扩展名打包，不用进行额外编码
+                    //[hash:8]：一个项目中如果两个文件夹中的图片重名，打包图片就会被覆盖，加上hash值的前八位作为图片名，可以避免重名。
+                }
+            }
             // {
             //     enforce: "pre",
             //     test: /\.js$/,
@@ -86,9 +57,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin({ //
-            filename: '[name].min.css' // 配置提取出来的css名称
-        })
+        // new ExtractTextPlugin({ //
+        //     filename: '[name].min.css' // 配置提取出来的css名称
+        // })
         // new webpack.WatchIgnorePlugin([/css\.d\.ts$/])
     ],
 }
